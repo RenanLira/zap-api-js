acec419f88c899d2efccfd0bf654cb731ea1fba9
RenanLira <renanalmeida2015.ra@gmail.com>
2022-07-05 17:57:15 -0300

    v1.0

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const path = require('path')
 const mime = require('mime-types');
 
-
-const lista = require('./lista')
 const {setdata} = require('./getjson')
 
 venom.create({
